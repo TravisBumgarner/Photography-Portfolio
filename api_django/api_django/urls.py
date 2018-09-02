cc6310a7bb9479cfca029aef8b022cb7a09cfabe
Travis Bumgarner <travis.bumgarner@gmail.com>
2018-09-02 10:51:31 -0400

    (api) Trying out Django

---
+++ api_django/api_django/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.conf.urls import url, include
+from django.urls import path
+
+urlpatterns = [
+    url(r'^api-auth/', include('rest_framework.urls')),
+    path('admin/', admin.site.urls),
+]
