85210834d61047d0f1877695cace12e6389dba2a
Travis Bumgarner <travis.bumgarner@gmail.com>
2018-09-02 10:57:44 -0400

    (api) Added models

--- api_django/photos/models.py
+++ api_django/photos/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    category = models.ManyToManyField("Category", blank=True)
+    description = models.TextField(blank=True, null=True)
+    start_date = models.DateField(null=True)
+    end_date = models.DateField(null=True)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Photo(models.Model):
+    src = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    project = models.ManyToManyField("Category", blank=True)
+    location = models.CharField(max_length=200)
+    width = models.IntegerField()
+    height = models.IntegerField()
+    meta = models.TextField()
+    date = models.DateField(null=True)   
