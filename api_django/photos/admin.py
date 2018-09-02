9c1bfe5bcee91ce9777ffad8b577a865d874b5e4
Travis Bumgarner <travis.bumgarner@gmail.com>
2018-09-02 11:02:21 -0400

    (api) hooked in admin

--- api_django/photos/admin.py
+++ api_django/photos/admin.py
@@ -1,3 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 from django.contrib import admin
 
-# Register your models here.
+from photos.models import *
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    pass
