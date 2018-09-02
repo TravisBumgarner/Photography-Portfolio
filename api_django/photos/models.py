c8e736dd213ab0ca06bf41710c2380e4dbc1e676
Travis Bumgarner <travis.bumgarner@gmail.com>
2018-09-02 11:05:24 -0400

    (api) fixed relations in models

--- api_django/photos/models.py
+++ api_django/photos/models.py
@@ -13,7 +13,7 @@ class Category(models.Model):
 
 class Project(models.Model):
     name = models.CharField(max_length=200, unique=True)
-    category = models.ManyToManyField("Category", blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
     description = models.TextField(blank=True, null=True)
     start_date = models.DateField(null=True)
     end_date = models.DateField(null=True)
@@ -25,7 +25,7 @@ class Project(models.Model):
 class Photo(models.Model):
     src = models.CharField(max_length=200)
     title = models.CharField(max_length=200)
-    project = models.ManyToManyField("Category", blank=True)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, null=True)
     location = models.CharField(max_length=200)
     width = models.IntegerField()
     height = models.IntegerField()
