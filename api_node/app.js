f21fea87081f27a7c44cc9ab2a1d63c55907282c
Travis Bumgarner <travis.bumgarner@gmail.com>
2018-09-05 11:00:18 -0400

    (global) removed node app

--- api_node/app.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-
-const app = express()
-
-app.get(`/ok`, (request, response) => {
-    response.send({ message: 'ok' })
-})
-
-export default app
--- api_node/server.js
+++
@@ -1,7 +0,0 @@
-import app from './app'
-
-const port = process.env.PORT || 8000
-
-app.listen(port, () => {
-    console.log(`Running on port ${port}`) // eslint-disable-line no-console
-})
