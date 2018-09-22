d8f1bd27edcc5c3f57f5a10f47bc7bbaf139ac35
Travis Bumgarner <travis.bumgarner@gmail.com>
2018-09-21 21:37:37 -0400

    (ui) removed resources

--- ui/src/resources/index.js
+++
@@ -1,5 +0,0 @@
-import leftArrowIcon from './chevron-left.svg'
-import rightArrowIcon from './chevron-right.svg'
-import closeIcon from './chevron-left.svg'
-
-export { leftArrowIcon, rightArrowIcon, closeIcon }
