2ee2775cda37ddbab58e26a41dd1fdcabb22cedd
Travis Bumgarner <travis.bumgarner@gmail.com>
2018-09-30 08:56:15 -0400

    (ui) Fixed styling issue

--- ui/src/containers/Post/Post.styles.js
+++ ui/src/containers/Post/Post.styles.js
@@ -5,7 +5,7 @@ import { FaTimes } from 'react-icons/fa'
 import { CONTENT_SPACING } from 'Theme'
 
 const PostWrapper = styled.div`
-    margin: ${CONTENT_SPACING.m} 0;
+    margin: 0 0 ${CONTENT_SPACING.m} 0;
 `
 
 export { PostWrapper }
