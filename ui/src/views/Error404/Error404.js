3ef72e5276dbf830c09611745504e396c121b190
Travis Bumgarner <travis.bumgarner@gmail.com>
2018-10-14 09:50:14 -0400

    (ui) Added Error404

---
+++ ui/src/views/Error404/Error404.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+import { Header, Text } from 'Components'
+
+import { Error404Wrapper } from './Error404.styles.js'
+
+const Error404 = () => {
+    return (
+        <Error404Wrapper>
+            <Header size="medium">Whoops!</Header>
+            <Text>The page you were looking for was not found.</Text>
+        </Error404Wrapper>
+    )
+}
+
+export default Error404
---
+++ ui/src/views/Error404/Error404.styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+import { PAGE_THEME } from 'Theme'
+
+const Error404Wrapper = styled(PAGE_THEME)`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+`
+
+export { Error404Wrapper }
