ef4819fe5b91875a8e503a1ec9f417f7babacccd
Travis Bumgarner <travis.bumgarner@gmail.com>
2019-11-21 13:49:27 -0500

    Refactored Home to Function Component

--- ui/src/utilities/index.js
+++ ui/src/utilities/index.js
@@ -29,4 +29,25 @@ const parseContent = rawContent => {
     })
 }
 
-export { parseContent }
+
+function useInterval(callback, delay) {
+    const savedCallback = React.useRef();
+  
+    // Remember the latest callback.
+    React.useEffect(() => {
+      savedCallback.current = callback;
+    }, [callback]);
+  
+    // Set up the interval.
+    React.useEffect(() => {
+      function tick() {
+        savedCallback.current();
+      }
+      if (delay !== null) {
+        let id = setInterval(tick, delay);
+        return () => clearInterval(id);
+      }
+    }, [delay]);
+  }
+
+export { parseContent, useInterval }
--- ui/src/views/Home/Home.js
+++ ui/src/views/Home/Home.js
@@ -1,58 +1,14 @@
-import React, { Component } from 'react'
+import React from 'react'
 
+import { useInterval } from 'Utilities'
 import { HomeWrapper } from './Home.styles.js'
 
-class Home extends Component {
-    constructor(props) {
-        super(props)
+const Home = ({ backgroundPhotos }) => {
+    const [backgroundImageIndex, setBackgroundImageIndex] = React.useState(0)
 
-        this.state = {
-            maxBgIndex: 0,
-            currentBgIndex: 0
-        }
-    }
-
-    componentDidMount() {
-        const { backgroundPhotos } = this.props
-        this.interval = window.setInterval(this.incrementBgIndex, 8000)
-        this.getPhotoCycleInfo(backgroundPhotos)
-    }
-
-    componentWillReceiveProps(nextProps) {
-        this.getPhotoCycleInfo(nextProps.backgroundPhotos)
-    }
-
-    componentWillUnmount() {
-        window.clearInterval(this.interval)
-    }
-
-    getPhotoCycleInfo = photos => {
-        const { maxBgIndex } = this.state
-        if (maxBgIndex !== photos.length - 1) {
-            const maxBgIndex = photos.length - 1
-
-            this.setState({
-                maxBgIndex,
-                currentBgIndex: Math.floor(Math.random() * maxBgIndex)
-            })
-        }
-    }
-
-    incrementBgIndex = () => {
-        const { maxBgIndex, currentBgIndex } = this.state
-        if (currentBgIndex === maxBgIndex) {
-            this.setState({ currentBgIndex: 0 })
-        } else {
-            this.setState({ currentBgIndex: this.state.currentBgIndex + 1 })
-        }
-    }
-
-    render() {
-        const { backgroundPhotos } = this.props
-        const { currentBgIndex } = this.state
-
-        return <HomeWrapper backgroundImageUrl={backgroundPhotos[currentBgIndex]} />
-    }
+    useInterval(() => setBackgroundImageIndex(backgroundImageIndex + 1), 4000);
+    
+    return <HomeWrapper backgroundImageUrl={backgroundPhotos[backgroundImageIndex % backgroundPhotos.length]} />
 }
 
 export default Home
