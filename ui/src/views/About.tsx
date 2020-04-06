38620f4bb799ad70681d8c4366ab13699bed0764
Travis Bumgarner <travis.bumgarner@gmail.com>
2020-04-06 12:32:52 -0400

    Updated home

---
+++ ui/src/views/About.tsx
@@ -0,0 +1,74 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import { Text, Header } from 'Components'
+import { PAGE_THEME, CONTENT_SPACING } from 'Theme'
+
+const ContentSection = styled.div`
+    flex: 1;
+`
+
+const ContentWrapper = styled.div`
+    display: flex;
+`
+const ContentSubWrapperLeft = styled.div`
+    max-width: 300px;
+    box-sizing: border-box;
+    padding-right: ${CONTENT_SPACING.m};
+`
+const ContentSubWrapperRight = styled.div`
+    padding-left: ${CONTENT_SPACING.m};
+    flex-grow: 1;
+`
+
+const Portrait = styled.img`
+    width: 100%;
+    height: auto;
+    margin-top: ${CONTENT_SPACING.xs};
+`
+
+const AboutWrapper = styled(PAGE_THEME)``
+
+const About = () => {
+    return (
+        <AboutWrapper>
+            <Header size="medium">About</Header>
+            <ContentWrapper>
+                <ContentSubWrapperLeft>
+                    <Portrait src="http://api.travisbumgarner.photography/static/images/travis.jpg" />
+                </ContentSubWrapperLeft>
+                <ContentSubWrapperRight>
+                    <ContentSection>
+                        <Header size="small">Location</Header>
+                        <Text>Boston, MA</Text>
+
+                        <Header size="small">Summary</Header>
+                        <Text>
+                            Software Engineer
+                            <br />
+                            Photographer
+                            <br />
+                            Maker
+                            <br />
+                            Designer
+                        </Text>
+
+                        <Header size="small">AWARDS & RECOGNITIONS</Header>
+                        <Text>
+                            2018 Noyes Museum of Art of Stockton University's RAW Exhibition
+                            <br />
+                            2017 1st Place BARPCV Photography Competition
+                            <br />
+                            2016 Front Page of National Geographic’s YourShot
+                            <br />
+                            2016 MIT Art Party
+                            <br />
+                        </Text>
+                    </ContentSection>
+                </ContentSubWrapperRight>
+            </ContentWrapper>
+        </AboutWrapper>
+    )
+}
+
+export default About
---
+++ ui/src/views/Home.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import { useInterval } from 'Utilities'
+import { PhotoType } from './App/App.types'
+import { PAGE_THEME } from 'Theme'
+
+const HomeWrapper = styled(PAGE_THEME)`
+    background-image: ${({ backgroundImageUrl }: { backgroundImageUrl: string }) => `url('https://storage.googleapis.com/photo21/photos/${backgroundImageUrl}')`};
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+    background-attachment: fixed;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    position: fixed;
+    z-index: 0;
+`
+
+type Props = {
+    backgroundPhotos: PhotoType[]
+}
+
+const Home = ({ backgroundPhotos }: Props) => {
+    const [backgroundImageIndex, setBackgroundImageIndex] = React.useState(0)
+
+    useInterval(() => setBackgroundImageIndex(backgroundImageIndex + 1), 4000);
+
+    return <HomeWrapper backgroundImageUrl={backgroundPhotos[backgroundImageIndex % backgroundPhotos.length].src} />
+}
+
+export default Home
