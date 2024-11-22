4033864e3cf61aac2ccfe25c3023e3ffdcca7acd
Travis Bumgarner <travis.bumgarner@gmail.com>
2024-11-21 23:17:47 -0600

    added some text hover

--- ui/src/components/GalleryPreview.tsx
+++ ui/src/components/GalleryPreview.tsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
-import { CONTENT_SPACING, FONT_SIZES } from 'theme'
+import { COLORS, CONTENT_SPACING, FONT_SIZES } from 'theme'
 import { context } from '../context'
 import { BlurImage } from '../sharedComponents'
 import { getPhotoUrl } from '../utils'
@@ -42,7 +42,11 @@ const GalleryPreview = ({
 const StyledLink = styled(Link)`
   text-decoration: none;
   text-align: center;
-  color: black;  
+  color: black;
+
+  &:hover {
+    color: ${COLORS.GREEN};
+  }
 `
 
 const Header = styled.h2`
