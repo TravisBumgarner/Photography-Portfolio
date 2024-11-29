0a1503814a4a625820c1ddb19b86c793a2b75b65
Travis Bumgarner <travis.bumgarner@gmail.com>
2024-11-29 14:04:19 -0600

    added loading indicator

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,10 +1,12 @@
 import React, { Suspense, lazy } from 'react'
 import { Route, Routes } from 'react-router-dom'
 
+import { motion } from 'framer-motion'
 import { Error } from 'src/sharedComponents'
 import styled from 'styled-components'
 import Context from './context'
 import './index.css'
+import Loading from './sharedComponents/Loading'
 import { MAX_WIDTH } from './theme'
 
 const About = lazy(async () => await import('./components/About'))
@@ -15,40 +17,43 @@ const Gallery = lazy(async () => await import('./components/Gallery'))
 
 const App = () => {
   return (
-    <AppContainer>
-      <TitleBar />
-      <Context>
-        <Navigation />
-        <Suspense fallback={<div>Loading...</div>}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/private/:gallerySlug">
-              <Route index element={<Gallery privateGallery={true} />} />
-              <Route
-                path=":photoSlug"
-                element={<Gallery privateGallery={true} />}
-              />
-            </Route>
-            <Route path="/:gallerySlug">
-              <Route index element={<Gallery privateGallery={false} />} />
-              <Route
-                path=":photoSlug"
-                element={<Gallery privateGallery={false} />}
-              />
-            </Route>
-            <Route path="/error500" element={<Error value="500" />} />
-            <Route path="*" element={<Error value="404" />} />
-          </Routes>
-        </Suspense>
-      </Context>
-    </AppContainer>
+    <>
+      <AppContainer animate={{ opacity: 1 }} transition={{ duration: 0.5 }}>
+        <TitleBar />
+        <Context>
+          <Navigation />
+          <Suspense fallback={<Loading />}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/private/:gallerySlug">
+                <Route index element={<Gallery privateGallery={true} />} />
+                <Route
+                  path=":photoSlug"
+                  element={<Gallery privateGallery={true} />}
+                />
+              </Route>
+              <Route path="/:gallerySlug">
+                <Route index element={<Gallery privateGallery={false} />} />
+                <Route
+                  path=":photoSlug"
+                  element={<Gallery privateGallery={false} />}
+                />
+              </Route>
+              <Route path="/error500" element={<Error value="500" />} />
+              <Route path="*" element={<Error value="404" />} />
+            </Routes>
+          </Suspense>
+        </Context>
+      </AppContainer>
+    </>
   )
 }
 
-const AppContainer = styled.div`
+const AppContainer = styled(motion.div)`
   max-width: ${MAX_WIDTH};
   margin: 0 auto;
+  opacity: 0;
 `
 
 export default App
---
+++ ui/src/sharedComponents/Loading.tsx
@@ -0,0 +1,73 @@
+import { motion } from 'framer-motion'
+import React from 'react'
+import { COLORS, Z_INDEX } from 'src/theme'
+import styled from 'styled-components'
+
+// const EXIT_DURATION = 1.5
+
+const LoadingContainer = styled(motion.div)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  background-color: ${COLORS.PRIMARY};
+  z-index: ${Z_INDEX.LOADING};
+`
+
+const Favicon = () => {
+  return (
+    <FaviconWrapper
+      animate={{ transform: 'rotate(360deg)' }}
+      transition={{ repeat: Infinity, duration: 3, ease: 'linear' }}
+      style={{ fill: COLORS.PRIMARY, width: 100, height: 100 }}
+      //   exit={{
+      // opacity: 0,
+      // transform: 'scale(20)',
+      // transition: { duration: EXIT_DURATION }
+      //   }}
+    >
+      <FaviconLeft />
+      <FaviconRight />
+    </FaviconWrapper>
+  )
+}
+
+const FaviconLeft = styled.div`
+  width: 50%;
+  height: 100%;
+  background-color: ${COLORS.BACKGROUND};
+  box-sizing: border-box;
+`
+
+const FaviconRight = styled.div`
+  width: 50%;
+  height: 100%;
+  background-color: ${COLORS.PRIMARY};
+  box-sizing: border-box;
+`
+
+const FaviconWrapper = styled(motion.div)`
+  width: 100px;
+  height: 100px;
+  fill: ${COLORS.PRIMARY};
+  border: 12px solid ${COLORS.BACKGROUND};
+  display: flex;
+  flex-direction: row;
+`
+
+const Loading = () => {
+  return (
+    <LoadingContainer
+    // Currently can't animate exit with Suspsense.
+    //   exit={{ opacity: 0, transition: { duration: EXIT_DURATION } }}
+    >
+      <Favicon />
+    </LoadingContainer>
+  )
+}
+
+export default Loading
