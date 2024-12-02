3ef11b8a421dc6819b7637c17e975e64dc44d9ea
Travis Bumgarner <travis.bumgarner@gmail.com>
2024-12-02 11:30:16 -0600

    cleaned up about

--- ui/src/components/About.tsx
+++ ui/src/components/About.tsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import styled from 'styled-components'
 
-import List from 'src/sharedComponents/List'
 import NavigationAnimation from 'src/sharedComponents/NavigationAnimation'
 import PageHeader from 'src/sharedComponents/PageHeader'
 import { CONTENT_SPACING } from 'src/theme'
 
 const ContentWrapper = styled.div`
   display: flex;
-  margin: ${CONTENT_SPACING.LARGE};
+  margin: ${CONTENT_SPACING.LARGE} 0;
   flex-direction: column;
 `
 
@@ -17,22 +16,31 @@ const About = () => {
     <NavigationAnimation>
       <ContentWrapper>
         <PageHeader>AWARDS & RECOGNITIONS</PageHeader>
-        <List
-          items={[
-            '2024 Master Class Selected',
-            '2024 March SA Monthly Contest Winner - Envelop',
-            '2023 April SA Monthly Contest Winner - Hard Light',
-            '2023 Black Box Gallery - Color Burst',
-            '2023 Praxis Gallery - The Portrait Exhibition',
-            '2018 Noyes Museum of Art of Stockton University - RAW Exhibition',
-            '2017 1st Place BARPCV Photography Competition',
-            "2016 Front Page of National Geographic's YourShot",
-            '2016 MIT Art Party'
-          ]}
-        />
+        <UL>
+          <li>2024 Master Class Selected</li>
+          <li>2024 March SA Monthly Contest Winner - Envelop</li>
+          <li>2023 April SA Monthly Contest Winner - Hard Light</li>
+          <li>2023 Black Box Gallery - Color Burst</li>
+          <li>2023 Praxis Gallery - The Portrait Exhibition</li>
+          <li>
+            2018 Noyes Museum of Art of Stockton University - RAW Exhibition
+          </li>
+          <li>2017 1st Place BARPCV Photography Competition</li>
+          <li>2016 Front Page of National Geographic&apos;s YourShot</li>
+          <li>2016 MIT Art Party</li>
+        </UL>
       </ContentWrapper>
     </NavigationAnimation>
   )
 }
+
+const UL = styled.ul`
+  margin: 0;
+  padding: 0;
+
+  li {
+    margin-bottom: ${CONTENT_SPACING.MEDIUM};
+  }
+`
 
 export default About
