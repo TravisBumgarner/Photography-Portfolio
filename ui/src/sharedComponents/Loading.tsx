71136f27474fb1d79d882afaf7c0c409ab21df98
Travis Bumgarner <travis.bumgarner@gmail.com>
2024-12-02 11:26:41 -0600

    added box shadow to loading icon

--- ui/src/sharedComponents/Loading.tsx
+++ ui/src/sharedComponents/Loading.tsx
@@ -52,6 +52,7 @@ const FaviconWrapper = styled(motion.div)`
   border: 12px solid ${COLORS.BACKGROUND};
   display: flex;
   flex-direction: row;
+  box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.1);
 `
 
 const Loading = () => {
