f46670d4cac65f845b158a6d3f88abce0813866b
Travis Bumgarner <travis.bumgarner@gmail.com>
2025-01-17 12:48:04 -0600

    implemented

--- ui/src/types.ts
+++ ui/src/types.ts
@@ -10,12 +10,6 @@ export interface PhotoType {
   src: string
   galleryIds: string[]
   dateTaken: string
-  // camera: string
-  // lens: string
-  // aperture: string
-  // shutterSpeed: string
-  // iso: string
-  // focalLength: string
   blurHash: string
   width: number
   height: number
