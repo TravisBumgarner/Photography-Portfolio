e3e223d7a7028d35c642f5e954d5a3e4ab24d848
Travis Bumgarner <11890057+TravisBumgarner@users.noreply.github.com>
2025-09-22 14:03:25 -0600

    Sam contest updates  (#95)

--- photography-store/survey-site-post-gcp/deploy.sh
+++ photography-store/survey-site-post-gcp/deploy.sh
@@ -1,3 +1,24 @@
-heroku create photo-survey -b https://github.com/mars/create-react-app-buildpack.git --stack heroku-24
-git push heroku main
-heroku open
+#!/bin/bash
+set -e
+
+APP_NAME="sam-photo-survey"
+BUILDPACK="https://github.com/mars/create-react-app-buildpack.git"
+STACK="heroku-24"
+
+echo ">>> Checking if Heroku app '$APP_NAME' exists..."
+if heroku apps:info --app "$APP_NAME" > /dev/null 2>&1; then
+  echo ">>> App already exists."
+else
+  echo ">>> Creating app '$APP_NAME'..."
+  heroku create "$APP_NAME" --buildpack "$BUILDPACK" --stack "$STACK"
+fi
+
+echo ">>> Ensuring git remote 'heroku' points to $APP_NAME..."
+heroku git:remote -a "$APP_NAME"
+
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+echo ">>> Pushing branch '$BRANCH' to Heroku..."
+git push heroku "$BRANCH:main"
+
+echo ">>> Opening app..."
+heroku open --app "$APP_NAME"
